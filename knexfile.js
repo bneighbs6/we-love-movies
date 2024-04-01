5cebe5c23001bea9acc55500278c0b994ee21090
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-03-31 20:59:42 -0600

    Update DATABASE_URL

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://kerjftam:mgqERCkg9sV4sJTHt47ycjuvjmGUetrH@bubble.db.elephantsql.com/kerjftam",
 } = process.env;
 
 module.exports = {
