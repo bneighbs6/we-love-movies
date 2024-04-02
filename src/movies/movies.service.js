c478a5313859998e80900671877debfd01f8c647
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-04-01 21:10:46 -0600

    create list() in movies service

---
+++ src/movies/movies.service.js
@@ -0,0 +1,10 @@
+const knex = require("../db/connection");
+
+function list() {
+    return knex("movies").select("*");
+}
+
+// READ HERE FIRST MOTHA FUCKA
+// You just successfully ran your seed files
+// You then created all necessary folders and  for theaters, reviews, and movies routes
+// Finish up the routes in each file
