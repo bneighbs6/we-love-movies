4b543f151454be03a00861bd0f7b58c5c6633770
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-04-02 11:12:43 -0600

    Exported list fx in service to get GET /movies to work

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -4,6 +4,10 @@ function list() {
     return knex("movies").select("*");
 }
 
+module.exports = {
+    list,
+}
+
 // READ HERE FIRST MOTHA FUCKA
 // You just successfully ran your seed files
 // You then created all necessary folders and  for theaters, reviews, and movies routes
