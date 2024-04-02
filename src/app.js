c57611d3b9610b985ead6891d67311ff24c75938
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-04-02 10:54:09 -0600

    Updated app.js; enabled cors & created base route for /movies

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors")
 const app = express();
+
+const moviesRouter = require("./movies/movies.router");
+
+// Enables CORS (cross origin resource sharing) across entire app
+app.use(cors());
+app.use(express.json());
+
+app.use("/movies", moviesRouter);
 
 module.exports = app;
