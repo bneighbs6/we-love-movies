44164245a3182c2efdd641624726cdbb5b6e4d0b
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-04-02 11:02:47 -0600

    Added list() to movies.controller.js

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,1 +1,12 @@
-const moviesService = require("./movies.service");
+const service = require("./movies.service");
+
+async function list(req, res) {
+    // Access Query parameter
+    const isShowing = req.query.is_showing;
+
+    // if (isShowing === "true") {
+    //     const data = await service.listMoviesShowing();
+    //     res.json({ data })
+    // }
+    res.json({ data: await service.list() })
+}
