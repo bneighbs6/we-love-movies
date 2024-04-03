27c6516098347def59ea80e7f42465aa2ef56149
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-04-03 08:31:58 -0600

    Placed /theaters route in app.js

--- src/app.js
+++ src/app.js
@@ -4,11 +4,14 @@ const cors = require("cors")
 const app = express();
 
 const moviesRouter = require("./movies/movies.router");
+const theatersRouter = require("./theaters/theaters.router");
 
 // Enables CORS (cross origin resource sharing) across entire app
 app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
+app.use("/theaters", theatersRouter);
+
 
 module.exports = app;
