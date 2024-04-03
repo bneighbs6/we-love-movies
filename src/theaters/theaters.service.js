e847430b7629c353a6226a7de11ad12d343b3597
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2024-04-03 08:30:25 -0600

    Created fx and route for route GET /theaters

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,3 +1,29 @@
+const knex = require("../db/connection");
+const reduceProperties = require("../utils/reduce-properties");
+
+// Creating nested movies array to include in the data returned by list()
+const reduceMovies = reduceProperties("theater_id", {
+  movie_id: ["movies", null, "movie_id"],
+  title: ["movies", null, "title"],
+  runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
+  rating: ["movies", null, "rating"],
+  description: ["movies", null, "description"],
+  image_url: ["movies", null, "image_url"],
+  is_showing: ["movies", null, "is_showing"],
+});
+
+function list() {
+    return knex("theaters as t")
+    .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
+    .join("movies as m", "mt.movie_id", "m.movie_id")
+    .select("t.*", "m.*", "mt.is_showing")
+    .then(reduceMovies);
+}
+
+module.exports = {
+    list,
+}
+
 // READ HERE FIRST MOTHA FUCKA
 // You just completed every route for "movies" table
 // Next start on the routes for "theaters" table
